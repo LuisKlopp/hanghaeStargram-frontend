3050894b9079e22a5c7a8980a2233ef26b3ad021
magicrain7 <magicrain88@gmail.com>
2022-08-20 10:00:59 +0900

    게시글작성 , 회원가입 페이지

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import { Routes, Route, Link, Redirect } from 'react-router-dom'
 import Login from './pages/Login'
 import Main from './pages/Main'
+import Signup from './pages/Signup';
+import { Newpost } from './components/Newpost';
 
 function App() {
 
@@ -11,6 +13,8 @@ function App() {
         <Routes>
         <Route path="/" element={<Main/>}/>
         <Route path="login" element={<Login />} />
+        <Route path="signup" element={<Signup />} />
+        <Route path="newpost" element={<Newpost />} />
         </Routes>
     </div>
   );
